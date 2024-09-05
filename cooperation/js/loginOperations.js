f433ed51a6e05eb47129022b38ffd4cf78175632
CampanulariaCoding <hannah@i40it.com>
2024-09-05 09:33:52 -0700

    login fix

--- cooperation/js/loginOperations.js
+++ cooperation/js/loginOperations.js
@@ -26,9 +26,9 @@ async function login() {
   //send request
   let response = await sendRequest("POST", data, loginUrl);
 
+  let resJson = await JSON.parse(response);
   console.log(resJson)
   console.log(resJson['token'])
-  let resJson = await JSON.parse(response);
 
   if (resJson['status'] == "Invalid") {
     document.getElementById("errorText").style.display = "block";
